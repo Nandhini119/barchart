13d5e338a23a4fa1b88fa70da2b5e136a88cf1a0
nandhini <nandhini.g21@wipro.com>
2017-06-07 07:23:00 +0000

    commit

--- test/datamunging-test.spec.js
+++ test/datamunging-test.spec.js
@@ -1,12 +1,12 @@
 let chai = require('chai');
 let expect = chai.expect;
-let out1 = require('../output/ageWiseLiterateDistribution');
-let out2 = require('../output/eduCategWise');
-let out3 = require('../output/gradPopStateAndGradeWise');
+//let out1 = require('../output/ageWiseLiterateDistribution');
+//let out2 = require('../output/eduCategWise');
+//let out3 = require('../output/gradPopStateAndGradeWise');
 let json1 = require('./json/ageWiseLiterateDistribution');
 let json2 = require('./json/eduCategWise');
 let json3 = require('./json/gradPopStateAndGradeWise');
-let jsoncompare = require('./jsoncompare.js');
+//let jsoncompare = require('./jsoncompare.js');
 describe('Test case for correct output', function() {
     it('Test whether the type of output folder is object or not', function(done) {
         expect(typeof json1).to.deep.equal('object');
